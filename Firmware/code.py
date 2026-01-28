baab21477d20af69141e6022a9d9d46462e79578
LizOnAir <choo.ke.ying@gmail.com>
2026-01-28 08:57:28 +0800

    Update code.py

--- Firmware/code.py
+++ Firmware/code.py
@@ -5,6 +5,9 @@ import board
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
+from kmk.modules.encoder import EncoderHandler
+from kmk.extensions.media_keys import MediaKeys
+from kmk.modules.mouse_keys import MouseKeys
 
 keyboard = KMKKeyboard()
 
@@ -12,11 +15,32 @@ keyboard.col_pins = (board.GPIO2, board.GPIO1, board.GPIO0, board.GPIO28)
 keyboard.row_pins = (board.GPIO0, board.GPIO4, board.GPIO3,)
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
+encoder = EncoderHandler()
+keyboard.modules.append(encoder)
+keyboard.modules.append(MediaKeys)
+keyboard.modules.append(MouseKeys)
+
+encoder.pins = ((board.GPIO26, board.GPIO27, None),)
+RAISE = KC.LT(1,KC.ESC)
+TRANS = KC.TRANS
+
 keyboard.keymap = [
-    [KC.LSHIFT,  KC.N1, KC.N2, KC.N3,
-    KC.KP_ENTER, KC.N4,  KC.N5,  KC.N6,
-    KC.TAB,  KC.N7,  KC.N8,  KC.N9
-  ]
+                    [RAISE,
+                    KC.LSHIFT,  KC.N1, KC.N2, KC.N3,
+                    KC.KP_ENTER, KC.N4,  KC.N5,  KC.N6,
+                    KC.TAB,  KC.N7,  KC.N8,  KC.N9
+                  ],
+                [
+                    [TRANS,
+                    KC.MUTE,  KC.MRWD, KC.MPLY, KC.MFFD,
+                    KC.KP_ENTER, KC.N4,  KC.N5,  KC.N6,
+                    KC.TAB,  KC.N7,  KC.N8,  KC.N9
+                  ],
+]
+encoder.keymap = [
+                    ((KC.VOLD, KC.VOLU),),
+                    ((KC.MW_DN,KC.MW_UP),),
+
 ]
 
 if __name__ == '__main__':
